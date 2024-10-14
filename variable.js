a33351ca38bd3bbdfba87e90439d5df6e55e205f
Monubhardwaj-tech <Monubhardwaj052@gmail.com>
2024-10-14 14:38:48 -0400

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,83 @@
+const product ={
+    title: "ball pen",
+    rating: 4,
+    offer: 5,
+    price: 270,
+};
+console.log(product);
+
+fullname ="tony stark"
+FULLNAME ="MONU"
+console.log(fullname);
+console.log(FULLNAME);
+
+// var:- variable can be re declared & updated.a global scope variable.
+
+// var age=24;
+// var age=59;
+// var age=86;
+// console.log("age")
+
+// let:- variable can not be re-declared but can be updated. a block scope variable.
+
+// let age=24;
+// age=59;
+// age=86;
+// console.log("age");
+
+// const:- A variable cannot be re-declared but can be updated . A block scope variable.(const is used in maths like pi = 3.14) mostly we prefer const bcz generally we use const value
+
+// var age = 50;
+
+// console.log(age)
+
+// console.log("legend monu");
+// console.log("i love javascript");
+// console.log("Monu bhardwaj");
+
+// fullname= "tarunbhardwaj";
+// console.log("fullname");
+
+// age=24;
+// price=500;
+// console.log(age);
+
+// {
+//    let a=5;
+//    console.log(a);
+// }
+// {
+//    let a =10;
+//    console.log(a);
+
+// }
+// // object data type
+// const student = {
+//    fullname : "rahul kumar",
+//    age : 20,
+//    cgpa : 8.2,
+//    ispass : true,
+
+// }
+
+// student["age"]=student["age"] + 1;
+// student["name"]= "rahul sharma";
+// console.log(student.name);
+// console.log(student.age);
+// console.log(student.cgpa);
+// console.log(student.ispass);
+
+let _a = 50;
+let _b = 50;
+console.log(_a + _b);
+
+// {
+//  let _a = 50;
+//  let _b = 7;
+
+// console.log( _a);}
+
+
+// let age1 = 59;
+// let age2 = 50;
+// console.log(age1);
