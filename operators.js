5a51c840ae119dd6d45dd49774f8f7dc5a1f5daa
Monubhardwaj-tech <Monubhardwaj052@gmail.com>
2024-10-14 14:46:10 -0400

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,56 @@
+//this is a code that print hello world
+// console.log("hello world")
+
+
+// operators used to perform some operations on data
+// arithmetic operators
+
+let a = 5;
+let b = 2;
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// // multiply operator
+// console.log("A*b",a*b);
+// // divide operator
+// console.log("a/b",a/b);
+// // modulus operator
+// console.log("a%b",a%b);
+// // exponential operator (power)
+// console.log("a**b",a**b);
+
+// unary operator   increment
+console.log("a=",a," & b = ", b);
+a = a + 1;  //a++
+console.log("a =", a);
+
+// unary operator post incerement (post says value change in next line)
+console.log("a",a,"&b",b);
+console.log("a++ = ",a++);
+console.log("a = ",a);
+
+
+// unary operator decrement
+console.log("a=",a, "& b = ", b);
+a = a-1; //a--
+console.log("a =",a);
+
+
+//unary operator post decrement
+console.log("a",a,"&",b);
+console.log("a-- =",a--);
+console.log("a = ",a);
+
+// aassignment operator
+a += 4; //a = a+4
+console.log("a =",a);
+
+// assignment operator
+a -= 4; //a = a-4
+console.log("a =",a);
+
+// assignment operator
+a **= 4; //  a = a ** 4
+console.log("a = ",a);
+
+
+
